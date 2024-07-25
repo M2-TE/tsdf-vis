40d1b9ac403d982170299bbb3465fb6e4b1f4b2a
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-07-25 10:43:50 +0200

    better sdl naming

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,18 @@ int SDL_AppInit(void** pp_appstate, int argc, char** argv) {
     *pp_appstate = new Engine();
     Engine* p_engine = static_cast<Engine*>(*pp_appstate);
     p_engine->init();
-    return 0;
+    return SDL_APP_CONTINUE;
 }
 int SDL_AppEvent(void* p_appstate, const SDL_Event* p_event) {
     Engine* p_engine = static_cast<Engine*>(p_appstate);
     p_engine->execute_event(p_event);
-    return 0;
+    return SDL_APP_CONTINUE;
 }
 int SDL_AppIterate(void* p_appstate) {
     Engine* p_engine = static_cast<Engine*>(p_appstate);
-    if (!p_engine->_running) return 1;
+    if (!p_engine->_running) return SDL_APP_SUCCESS;
     if (p_engine->_rendering) p_engine->execute_frame();
-    return 0;
+    return SDL_APP_CONTINUE;
 }
 void SDL_AppQuit(void* p_appstate) {
     Engine* p_engine = static_cast<Engine*>(p_appstate);
