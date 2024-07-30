0b5cb9b6bfe5af4e59c0a2a3c5e55ff565d9a841
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-07-30 18:33:39 +0200

    update of deps

--- cmake/vk.cmake
+++ cmake/vk.cmake
@@ -1,5 +1,5 @@
-FetchContent_Declare(vk GIT_REPOSITORY "https://github.com/KhronosGroup/Vulkan-Headers.git" GIT_TAG "v1.3.280" GIT_SHALLOW ON GIT_SUBMODULES "" SOURCE_SUBDIR "disabled/")
-FetchContent_Declare(vk_hpp GIT_REPOSITORY "https://github.com/KhronosGroup/Vulkan-Hpp.git" GIT_TAG "v1.3.280" GIT_SHALLOW ON GIT_SUBMODULES "" SOURCE_SUBDIR "disabled/")
+FetchContent_Declare(vk GIT_REPOSITORY "https://github.com/KhronosGroup/Vulkan-Headers.git" GIT_TAG "v1.3.290" GIT_SHALLOW ON GIT_SUBMODULES "" SOURCE_SUBDIR "disabled/")
+FetchContent_Declare(vk_hpp GIT_REPOSITORY "https://github.com/KhronosGroup/Vulkan-Hpp.git" GIT_TAG "v1.3.290" GIT_SHALLOW ON GIT_SUBMODULES "" SOURCE_SUBDIR "disabled/")
 FetchContent_MakeAvailable(vk vk_hpp)
 target_include_directories(${PROJECT_NAME} SYSTEM PRIVATE
     "${vk_SOURCE_DIR}/include/"
