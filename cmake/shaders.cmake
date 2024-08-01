7e129c130e601576edc259067b4cacaadf5da2f5
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-08-01 08:42:27 +0200

    sync

--- cmake/shaders.cmake
+++ cmake/shaders.cmake
@@ -50,7 +50,7 @@ foreach(GLSL_FILE ${GLSL_SOURCE_FILES})
     add_custom_command(
         COMMENT "Compiling shader: ${FILE_NAME}"
         OUTPUT  "${SPIRV_FILE}"
-        COMMAND "${GLSLANG_BINARY_DIR}/$<CONFIG>/glslang" -V "${GLSL_FILE}" -o "${SPIRV_FILE}"
+        COMMAND glslang-standalone -V "${GLSL_FILE}" -o "${SPIRV_FILE}"
         DEPENDS "${GLSL_FILE}" glslang-standalone)
     list(APPEND SPIRV_BINARY_FILES "${SPIRV_FILE}")
 endforeach(GLSL_FILE)
