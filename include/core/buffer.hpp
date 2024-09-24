98a02c1eb071bb396dada1660c3f37c5366da957
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-09-24 16:14:39 +0200

    buffer cleanup

--- include/core/buffer.hpp
+++ include/core/buffer.hpp
@@ -6,7 +6,6 @@ template<typename T>
 struct DeviceBuffer {
 	void init(vma::Allocator vmalloc, const vk::BufferCreateInfo& info_buffer, const vma::AllocationCreateInfo& info_allocation) {
 		std::tie(_data, _allocation) = vmalloc.createBuffer(info_buffer, info_allocation);
-		_mapped_data_p = vmalloc.mapMemory(_allocation);
 
 		// check for host coherency and visibility
 		vk::MemoryPropertyFlags props = vmalloc.getAllocationMemoryProperties(_allocation);
@@ -23,18 +22,36 @@ struct DeviceBuffer {
 	auto static constexpr size() -> vk::DeviceSize {
 		return sizeof(T);
 	}
-	void write(vma::Allocator vmalloc, const T& data) {
-		if (_require_staging) fmt::println("ReBAR is currently required and not present");
-		std::memcpy(_mapped_data_p, &data, sizeof(T));
+	auto map(vma::Allocator vmalloc) -> void* {
+		if (_require_staging) fmt::println("ReBAR required, staging buffer not yet implemented");
+		return vmalloc.mapMemory(_allocation);
+	}
+	void write(vma::Allocator vmalloc, T& data) {
+		if (_require_staging) fmt::println("ReBAR required, staging buffer not yet implemented");
+		void* map_p = vmalloc.mapMemory(_allocation);
+		std::memcpy(map_p, &data, sizeof(T));
+		vmalloc.unmapMemory(_allocation);
 		if (_require_flushing) vmalloc.flushAllocation(_allocation, 0, sizeof(T));
 	}
-	void write_partial() {/*TODO*/}
+	void write(vma::Allocator vmalloc, T& data, void* map_p) {
+		std::memcpy(map_p, &data, sizeof(T));
+		if (_require_flushing) vmalloc.flushAllocation(_allocation, 0, sizeof(T));
+	}
+	void write(vma::Allocator vmalloc, size_t dst_offset, size_t size, void* data) {
+		if (_require_staging) fmt::println("ReBAR required, staging buffer not yet implemented");
+		void* map_p = vmalloc.mapMemory(_allocation);
+		std::memcpy((char*)map_p + dst_offset, data, size);
+		vmalloc.unmapMemory(_allocation);
+		if (_require_flushing) vmalloc.flushAllocation(_allocation, dst_offset, size);
+	}
+	void write(vma::Allocator vmalloc, size_t dst_offset, size_t size, void* data, void* map_p) {
+		std::memcpy((char*)map_p + dst_offset, data, size);
+		vmalloc.unmapMemory(_allocation);
+		if (_require_flushing) vmalloc.flushAllocation(_allocation, dst_offset, size);
+	}
 
 	vk::Buffer _data;
 	vma::Allocation _allocation;
-	// todo: is there a cost to leaving resources mapped?
-	// todo: no need to map when staging buffer is required anyways
-	void* _mapped_data_p;
 	bool _require_staging;
 	bool _require_flushing;
 };
