528af33ed1bd99c8dad2da9d587325e4cd3399b5
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-09-24 12:59:37 +0200

    glm makeover

--- cmake/glm.cmake
+++ cmake/glm.cmake
@@ -1,23 +1,20 @@
-if(${CROSS_PLATFORM_DETERMINISTIC})
-    set(GLM_ENABLE_FAST_MATH OFF)
-else()
-    set(GLM_ENABLE_FAST_MATH ON)
-endif()
+
 if(${CMAKE_CXX_STANDARD} GREATER_EQUAL 20)
     set(GLM_ENABLE_CXX_20 ON)
-else()
-    set(GLM_ENABLE_CXX_20 OFF)
+elseif(${CMAKE_CXX_STANDARD} EQUAL 17)
+    set(GLM_ENABLE_CXX_17 ON)
+elseif(${CMAKE_CXX_STANDARD} EQUAL 14)
+    set(GLM_ENABLE_CXX_14 ON)
+elseif(${CMAKE_CXX_STANDARD} EQUAL 11)
+    set(GLM_ENABLE_CXX_11 ON)
 endif()
-set(GLM_DISABLE_AUTO_DETECTION  OFF)
-set(GLM_ENABLE_LANG_EXTENSIONS  ${CMAKE_CXX_EXTENSIONS})
-set(GLM_FORCE_PURE              OFF)
 
 # query SIMD support
 FetchContent_Declare(libsimdpp GIT_REPOSITORY "https://github.com/p12tic/libsimdpp.git" GIT_TAG "master" GIT_SHALLOW ON SOURCE_SUBDIR "disabled/")
 FetchContent_MakeAvailable(libsimdpp)
 list(APPEND CMAKE_MODULE_PATH "${libsimdpp_SOURCE_DIR}/cmake/")
 include(SimdppMultiarch)
-simdpp_get_runnable_archs(archs)
+simdpp_get_runnable_archs(ARCHS)
 # use SIMD support info to configure glm
 set(GLM_ENABLE_SIMD_SSE         ${CAN_RUN_X86_SSE})
 set(GLM_ENABLE_SIMD_SSE2        ${CAN_RUN_X86_SSE2})
@@ -27,10 +24,23 @@ set(GLM_ENABLE_SIMD_SSE4_1      ${CAN_RUN_X86_SSE4_1})
 set(GLM_ENABLE_SIMD_SSE4_2      ${CAN_RUN_X86_SSE4_2})
 set(GLM_ENABLE_SIMD_AVX         ${CAN_RUN_X86_AVX})
 set(GLM_ENABLE_SIMD_AVX2        ${CAN_RUN_X86_AVX2})
-set(GLM_ENABLE_SIMD_AVX512F     ${CAN_RUN_X86_AVX512F})
+
+# standard glm options
+set(GLM_BUILD_LIBRARY ON)
+set(GLM_BUILD_TEST OFF)
+set(GLM_BUILD_INSTALL OFF)
+if(${CROSS_PLATFORM_DETERMINISTIC})
+    set(GLM_ENABLE_FAST_MATH OFF)
+else()
+    set(GLM_ENABLE_FAST_MATH ON)
+endif()
+set(GLM_ENABLE_LANG_EXTENSIONS ${CMAKE_CXX_EXTENSIONS})
+set(GLM_DISABLE_AUTO_DETECTION OFF)
+set(GLM_FORCE_PURE OFF)
+
 FetchContent_Declare(glm GIT_REPOSITORY "https://github.com/g-truc/glm.git" GIT_TAG "1.0.1" GIT_SHALLOW ON)
 FetchContent_MakeAvailable(glm)
-target_compile_definitions(${PROJECT_NAME} PRIVATE 
+target_compile_definitions(${PROJECT_NAME} PRIVATE
     "GLM_FORCE_DEPTH_ZERO_TO_ONE"
     "GLM_FORCE_ALIGNED_GENTYPES"
     "GLM_FORCE_INTRINSICS")
--- include/components/transform/camera.hpp
+++ include/components/transform/camera.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/euler_angles.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_aligned.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <vulkan/vulkan.hpp>
 #include <vk_mem_alloc.hpp>
 #include <fmt/base.h>
@@ -8,7 +9,7 @@
 
 struct Camera {
 	struct BufferData {
-		glm::mat4x4 matrix;
+		glm::aligned_mat4x4 matrix;
 	};
     void init(vma::Allocator vmalloc, const vk::ArrayProxy<uint32_t>& queues) {
         // create camera matrix buffer
@@ -55,23 +56,25 @@ struct Camera {
 		if (Keys::down(SDLK_LSHIFT)) speed *= 8.0;
 
 		// move in direction relative to camera
-		glm::quat q_rot(_rot);
-		if (Keys::down('w')) _pos += q_rot * glm::vec3(0, 0, +speed);
-		if (Keys::down('s')) _pos += q_rot * glm::vec3(0, 0, -speed);
-		if (Keys::down('d')) _pos += q_rot * glm::vec3(+speed, 0, 0);
-		if (Keys::down('a')) _pos += q_rot * glm::vec3(-speed, 0, 0);
-		if (Keys::down('q')) _pos += q_rot * glm::vec3(0, +speed, 0);
-		if (Keys::down('e')) _pos += q_rot * glm::vec3(0, -speed, 0);
+		glm::qua<float, glm::aligned_highp> q_rot(_rot);
+		if (Keys::down('w')) _pos += q_rot * glm::aligned_vec3(0, 0, +speed);
+		if (Keys::down('s')) _pos += q_rot * glm::aligned_vec3(0, 0, -speed);
+		if (Keys::down('d')) _pos += q_rot * glm::aligned_vec3(+speed, 0, 0);
+		if (Keys::down('a')) _pos += q_rot * glm::aligned_vec3(-speed, 0, 0);
+		if (Keys::down('q')) _pos += q_rot * glm::aligned_vec3(0, +speed, 0);
+		if (Keys::down('e')) _pos += q_rot * glm::aligned_vec3(0, -speed, 0);
 
 		// only control camera when mouse is captured
 		if (Mouse::captured()) {
-			_rot += glm::vec3(-Mouse::delta().second, +Mouse::delta().first, 0) * 0.005f;
+			_rot += glm::aligned_vec3(-Mouse::delta().second, +Mouse::delta().first, 0) * 0.005f;
 		}
 
 		// merge rotation and projection matrices
 		BufferData data;
 		data.matrix = glm::perspectiveFovLH<float>(glm::radians<float>(_fov), _extent.width, _extent.height, _near, _far);
-		data.matrix = data.matrix * glm::eulerAngleXY(-_rot.x, -_rot.y);
+		data.matrix = glm::rotate(data.matrix, -_rot.x, glm::aligned_vec3(1, 0, 0));
+		data.matrix = glm::rotate(data.matrix, -_rot.y, glm::aligned_vec3(0, 1, 0));
+
 		data.matrix = glm::translate(data.matrix, -_pos);
 		
 		// upload data
@@ -79,11 +82,11 @@ struct Camera {
 		std::memcpy(_mapped_data_p, &data, sizeof(BufferData));
 		if (_require_flushing) vmalloc.flushAllocation(_allocation, 0, sizeof(BufferData));
 	}
-    
+
 	// cpu related
+	glm::aligned_vec3 _pos = { 0, 0, 0 };
+	glm::aligned_vec3 _rot = { 0, 0, 0 };
 	vk::Extent2D _extent;
-	glm::vec3 _pos = { 0, 0, 0 };
-	glm::vec3 _rot = { 0, 0, 0 };
 	float _fov = 60;
 	float _near = 0.01;
 	float _far = 1000.0;
